b9f0145999449ebd08b3516a3328937a6ad29176
Bruno Bigras <bigras.bruno@gmail.com>
2022-02-01 16:31:38 -0500

    normal graphql query

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -1,2 +1,26 @@
+<script>
+  import { operationStore, query, createClient, setClient, defaultExchanges, dedupExchange, fetchExchange, cacheExchange } from '@urql/svelte';
+
+  const client = createClient({
+	url: "http://localhost:8000/graphql",
+	exchanges: [
+      dedupExchange,
+      cacheExchange,
+      fetchExchange
+    ]
+  });
+
+  setClient(client);
+
+  const todos = operationStore(`
+    query {
+ 	value
+}`);
+
+  query(todos);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+value: {$todos.data && $todos.data.value}
